19a09d31682ec6d81cc4ce208a1c4c329f7a2587
lmolinar <lokomotiv.13@hotmail.it>
2024-12-15 01:16:54 +0100

    Setup root layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,42 +1,46 @@
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  Meta,
-  MetaFunction,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import "react-toastify/dist/ReactToastify.css";
+
+import { LinksFunction } from "@remix-run/node";
+import { Link, Links, Meta, MetaFunction, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import fonts from "inter-ui/inter.css?url";
+import { ToastContainer } from "react-toastify";
+
 import appStylesHref from "./app.css?url";
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: appStylesHref },
-  {
-    rel: "icon",
-    href: "/favicon-32x32.png"
-  },
+    { rel: "stylesheet", href: fonts },
+    { rel: "stylesheet", href: appStylesHref },
+    {
+        rel: "icon",
+        href: "/favicon-32x32.png",
+    },
 ];
 
-export const meta: MetaFunction = () => [
-    {title: "Team Management - Prisma"}
-]
+export const meta: MetaFunction = () => [{ title: "Team Management - Prisma" }];
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <div id="root-content">
-          <Outlet />
-        </div>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <ToastContainer />
+                <header className="header">
+                    <Link to="/" style={{ display: "flex", height: 40 }}>
+                        <img src="/prisma.svg" alt="Prisma logo" />
+                    </Link>
+                    <h1>Team Management - Prisma</h1>
+                </header>
+                <div id="rootContent">
+                    <Outlet />
+                </div>
+                <ScrollRestoration />
+                <Scripts />
+            </body>
+        </html>
+    );
 }
