bfecf681f5bde226528619b93ec5c153f5f45799
lmolinar <lokomotiv.13@hotmail.it>
2024-12-15 15:16:15 +0100

    Simplify production version run

--- app/db/db.server.ts
+++ app/db/db.server.ts
@@ -4,7 +4,7 @@ const { Pool } = pg;
 // Create a new pool of database connections
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL, // Use an environment variable for the database URL
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    ssl: false,
 });
 
 // Close the pool when the app shuts down
